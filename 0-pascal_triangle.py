903e7782db2c2e015a26ee04a1fb6284daa6096e
robertokoba7 <okobarobert7@gmail.com>
2023-03-24 01:59:55 +0300

    [add]: generate the Pascal's triangle of the given input n

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -11,23 +11,39 @@ def pascal_triangle(n):
     Args:
         n (int): Size of the Pascal's triangle
     """
-    lst = []
-    if (n <= 0):
-        return lst
-    else:
-        for i in range(n+1):
-            tmp = []
+#    lst = []
+ #   if (n <= 0):
+  #      return lst
+   # else:
+    #    for i in range(n+1):
+     #       tmp = []
             # first element is always 1 
-            elem = 1
-            for j in range(1, i+1):
+      #      elem = 1
+       #     for j in range(1, i+1):
 
 # first value in the line is always 1 
-                tmp.append(elem)
+        #        tmp.append(elem)
                 # using Binomial Coeff
 
-                elem = elem * (i - j) // j
-        if (len(tmp)):
-            lst.append(tmp)
+         #       elem = elem * (i - j) // j
+        #if (len(tmp)):
+         #   lst.append(tmp)
 
-    return lst
+    #return lst
 
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    matrix = []
+    if n <= 0:
+        return matrix
+    for i in range(n):
+        arr = []
+        for j in range(i+1):
+            if j == 0:
+                arr.append(1)
+            elif j == i:
+                arr.append(1)
+            else:
+                arr.append(matrix[i-1][j-1] + matrix[i-1][j])
+        matrix.append(arr)
+    return matrix
