2736000c60d1fad679e43d8249ed2aa983326d4c
robertokoba7 <okobarobert7@gmail.com>
2023-03-24 02:23:30 +0300

    [add]: generate the Pascal's triangle of the given input n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-"""The concept tests a triangular array of binomial coefficients, where each element of the triangle is the sum of the two elements directly above it also referred to as Pascal's Triangle
+"""
+The concept tests a triangular array of binomial coefficients, where each element of the triangle is the sum of the two elements directly above it also referred to as Pascal's Triangle
 """
 
 
