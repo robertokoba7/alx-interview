5cfee61f38ba0fdedb7d4e3a390b3d4d38d09f2e
robertokoba7 <okobarobert7@gmail.com>
2023-03-24 02:21:49 +0300

    [add]: generate the Pascal's triangle of the given input n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,21 +7,19 @@
 def pascal_triangle(n):
     """ returns a list of lists of integers representing the Pascal’s triangle of n
     """
-
-
-if type(n) is not int:
+    if not isinstance(n, int):
         raise TypeError("n must be an integer")
     matrix = []
     if n <= 0:
         return matrix
     for i in range(n):
         arr = []
-        for j in range(i+1):
+        for j in range(i + 1):
             if j == 0:
                 arr.append(1)
             elif j == i:
                 arr.append(1)
             else:
-                arr.append(matrix[i-1][j-1] + matrix[i-1][j])
+                arr.append(matrix[i - 1][j - 1] + matrix[i - 1][j])
         matrix.append(arr)
     return matrix
