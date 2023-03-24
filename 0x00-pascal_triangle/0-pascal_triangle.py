07b6485b3aa479b7f436c02ee83ef6017bf846d6
robertokoba7 <okobarobert7@gmail.com>
2023-03-24 11:20:59 +0300

    list of lists of integers representing the Pascal’s triangle of n returned

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,11 +5,6 @@ The concept tests a triangular array of binomial coefficient"""
 
 def pascal_triangle(n):
     """ returns a list of lists of integers representing the Pascal’s triangle of n.
-    parameters:
-        n [int]:
-            the number of rows
-    return:
-        [list of lists of ints]:
     """
     if not isinstance(n, int):
         raise TypeError("n must be an integer")
