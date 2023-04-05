1aa187eb70ddf7daa146cec02e2e523415cf86e5
robertokoba7 <okobarobert7@gmail.com>
2023-04-05 17:40:42 +0300

    [add] 2 keys keyboard to determine minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,25 +12,26 @@ def minOperations(n):
                 Otherwise 0, if impossible to generate n H char
     """
     # Initialize the variables current, count, buffer1 to run dynamic
-    current = 1
-    count = 0
-    buffer1 = 0
-    # when current is less than n we return the remaining number of times H.
+    current = 1 # current number of characters in the file
+    count = 0 # minimum number of operations required
+    buffer1 = 0 # number of characters to copy in a single operation
+
+    # Continue until the number of characters in the file is equal to n
     while current < n:
         rest = n - current
 
-        # Check if we can generate a sequence of H char
+        # Check if we can generate a sequence of H characters with the current buffer1
         if(rest % current == 0):
             buffer1 = current
-            current += buffer1
-            count += 2
+            current += buffer1 # copy and paste buffer1  number of characters
+            count += 2 # two operations (copy and paste operation) performed
 
         else:
-            current += buffer1
-            count += 1
+            current += buffer1 # paste buffer1 number of charactes
+            count += 1 # one operation (paste) perfomed
 
-    # Otherwise, return minimum number of operations
+    # Check if the resulting number of characters in the file is equal to n
     if current == n:
-        return count
+        return count # return minimum number of operations required
     else:
-        return 0
+        return 0 # return 0 if it is impossible to achieve n H characters
