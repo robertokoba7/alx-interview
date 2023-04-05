4671819dddea46b9982421b8b86af203055258d7
robertokoba7 <okobarobert7@gmail.com>
2023-04-05 17:00:47 +0300

    [add] 2 keys keyboard to determine minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import math
-
-
-def minOPerations(self, n):
+"""
+2 key keyboard interview problem
+"""
+def minOperations(self, n):
     """
     Given:
         Current number: n
@@ -10,14 +10,13 @@ def minOPerations(self, n):
                 Otherwise 0, if impossible to generate n H char
     """
     # Initialize the variables current, count, buffer1 to run dynamic
-    # programming
     current = 1
     count = 0
     buffer1 = 0
     # when current is less than n we return the remaining number of times H
     # occur
     while current < n:
-        rest = n - current
+        rest = n - current # 
 
         # Check if we can generate a sequence of H char
         if(rest % current == 0):
