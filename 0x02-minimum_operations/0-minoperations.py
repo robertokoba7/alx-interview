3ff2c8bdc5fc6561317e256e4f741789e87b25bc
robertokoba7 <okobarobert7@gmail.com>
2023-04-05 12:30:00 +0300

    [add] 2-keys keyboard

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -27,20 +27,3 @@ def minOPerations(n):
         return 0
     # Otherwise, return mininumber of operations
     return lst[n]
-
-
-
-
-
-
-
-import math
-
-def minOperations(n: int) -> int:
-    dp = [math.inf] * (n+1)
-    dp[1] = 0
-    for i in range(2, n+1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + (i // j))
-    return 0 if dp[n] == math.inf else dp[n]
