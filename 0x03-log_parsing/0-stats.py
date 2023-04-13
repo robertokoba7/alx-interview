f21f1c5333fe52391264d94ac3424ccd776a6a11
robertokoba7 <okobarobert7@gmail.com>
2023-04-13 19:04:07 +0300

    [add]: Read log files that contain data in Apache log file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,32 +2,34 @@
 import sys
 
 # metric variables initialized
-total_size = 0
-status_counts = {}
+total_file_size = 0
+line_count = 0
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-try:
-    # Read input data from stdin
-    for i, line in enumerate(sys.stdin):
-        # Parse input line when matching the expected format
-        try:
-            ip_address, _,_,_,status_code, file_size = line.split()
-            status_code = int(status_code)
-            file_size = int(file_size)
-            file_size = int(file_size)
-        except ValueError:
-            # Skip line if it unmatched the format expected
-            continue
+# Read input data from stdin
+for line in sys.stdin:
+    # Parse input line when matching the expected format
+    try:
+        ip_address, _, _, _, timestamp, _, request, status_code, file_size = line.split().split()
+    except ValueError:
+        # Skip line if it unmatched the format expected
+        continue
 
-        # Print metrics every 10 lines of input
-        if i % 10 == 9:
-            print("File size:", total_size)
-            for code in sorted(status_counts.keys()):
-                print(code, ":", status_counts[code])
+        # Update total file size and status code counts
+        total_file_size += int(file_size)
+        status_code = int(status_code)
+        if status_code in status_code_counts:
+            status_code_counts[status_code] += 1
 
-except KeyboardInterrupt:
-    # Print final metric
-    print("File size:", total_size)
-    for code in sorted(status_counts.keys()):
-        print(code, ":", status_counts[code])
+        # Increment line count
+        line_count += 1
 
+
+        # Print metrics evry 10 lines or on keyboard interrupt
+        if -count % 10 == 0:
+            print(f"File size:, {total_file_size}")
+            for code, count in sorted(status_code_counts.items()):
+                if count > 0:
+                print(f"{code}, {count}")
+            print()
