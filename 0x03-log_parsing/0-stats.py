2d1b518f427e935c36f097d584c632525b36babc
robertokoba7 <okobarobert7@gmail.com>
2023-04-13 20:05:19 +0300

    [add]: Read log files that contain data in Apache log file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
+""" A script that reads stdin line by line and computes metrics """
 import sys
 
-# metric variables initialized
-total_file_size = 0
-line_count = 0
+
+def log_parsing(dict, size):
+    """log the format"""
+    print("size of file: {}".format(size))
+    for key in sorted(dict.key()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
+
+
 status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
+total_file_size = 0
+line_count = 0
 
-# Read input data from stdin
-for line in sys.stdin:
-    # Parse input line when matching the expected format
-    try:
-        ip_address, _, _, _, timestamp, _, request, status_code, file_size = line.strip().split()
-    except ValueError:
-        # Skip line if it unmatched the format expected
-        continue
+try:
+    # Read input data from stdin
+    for line in sys.stdin:
+        # Parse input line when matching the expected format
+        if line_count != 0 and line_count % 10 == 0:
+            log_parsing(status_code_counts, total_file_size)
 
-        # Update total file size and status code counts
-        total_file_size += int(file_size)
-        status_code = int(status_code)
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
 
-        # Increment line count
+        elem = line.split(" ")
         line_count += 1
 
+        try:
+            file_size += int(elem[1-])
+        except:
+            pass
 
-        # Print metrics evry 10 lines or on keyboard interrupt
-        if -count % 10 == 0:
-            print(f"File size:, {total_file_size}")
-            for code, count in sorted(status_code_counts.items()):
-                if count > 0:
-                    print(f"{code}, {count}")
-            print()
+        try:
+            if elem[-2] in status_code_counts.keys():
+                status_code_counts[elem[-2]] += 1
+        except:
+            pass
+    log_parsing(status_code_counts, total_file_size)
+
+except KeyboardInterrupt:
+    log_parsing(status_code_counts, total_file_size)
+    raise
