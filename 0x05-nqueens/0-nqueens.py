74954af9ae92769f840b506b3b07d5ada378490e
robertokoba7 <okobarobert7@gmail.com>
2023-04-26 18:06:46 +0300

    [add]:Backtracking algorithm in nqueen implemantation

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,78 @@
 #!/usr/bin/python3
 """
-BACKTRACKING ALGORITHM IN SOLVING N QUEENS.
+BACKTRACKING ALGORITHM IN SOLVING N-QUEENS.
 """
 import sys
 
-
 class NQueens:
     """
-    implementation of n-queens using backtrackin alorithm
+    Implementation of nqueen 
     """
-
     def __init__(self, N):
-        # Initialize the board size and an empty board list
         self.N = N
-        self.board = [-1] * N
+        self.board = [['.' for _ in range(N)] for _ in range(N)]
 
-    def is_safe(self, i, j):
-        # Check if it's safe to place a queen at row i and column j
-        for row in range(i):
-            if self.board[row] == j or self.board[row] - j == row - i or self.board[row] - j == i - row:
+    def is_safe(self, row, col):
+        # Check if there is a queen in the same column
+        for i in range(row):
+            if self.board[i][col] == 'Q':
                 return False
+
+        # Check if there is a queen in the upper-left diagonal
+        for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
+            if self.board[i][j] == 'Q':
+                return False
+
+        # Check if there is a queen in the upper-right diagonal
+        for i, j in zip(range(row-1, -1, -1), range(col+1, self.N)):
+            if self.board[i][j] == 'Q':
+                return False
+
+        # If all checks pass, it is safe to place a queen at this position
         return True
 
-    def place_queen(self, i):
-        # Recursive function to place queens one by one
-        if i == self.N:
-            # All queens have been placed successfully
+    def place_queen(self, row):
+        # Base case: if all rows are filled, a valid solution is found
+        if row == self.N:
             return True
-        for j in range(self.N):
-            if self.is_safe(i, j):
-                # Place the queen at row i and column j
-                self.board[i] = j
-                # Try placing the next queen
-                if self.place_queen(i+1):
+
+        # Try to place a queen in each column of the current row
+        for col in range(self.N):
+            if self.is_safe(row, col):
+                self.board[row][col] = 'Q'
+                if self.place_queen(row+1):
                     return True
-                # Backtrack if placing the next queen didn't work
-                self.board[i] = -1
-        # No safe placement found for this row
+                self.board[row][col] = '.'
+
+        # If no valid solution is found, backtrack
         return False
 
     def solve(self):
-        # Call the recursive function to find the solution
         if self.place_queen(0):
-            # Print the board with "Q" representing queens and "." representing empty spaces
-            for row in range(self.N):
-                for col in range(self.N):
-                    if self.board[row] == col:
-                        print("Q", end=" ")
-                    else:
-                        print(".", end=" ")
-                print()
+            # Print the solution
+            for row in self.board:
+                print(' '.join(row))
         else:
-            print("No solution found")
+            print('No solutions found.')
+
+if __name__ == '__main__':
+    # Parse the command-line argument N
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+
+    if N < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    # Create an instance of the NQueens class with the given size
+    nqueens = NQueens(N)
+
+    # Call the solve method to solve the problem and print the solution
+    nqueens.solve()
